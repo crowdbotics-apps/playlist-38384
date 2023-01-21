013855ff99605e3b514381b6f85ad4e81b8f3790
Crowdbotics <engineering@crowdbotics.com>
2023-01-21 22:25:37 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -5,8 +5,21 @@ export const globalOptions = {
   url: "https://playlist-38384.botics.co",
   api: "https://playlist-38384.botics.co/api/v1"
 }
+
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
+  },
+  undefined: {
+    LOGO_URL:
+      "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/cb-icon.png",
+    BACKGROUND_URL:
+      "https://labs.openai.com/e/oqJnpgMqdGj4deAtKath9O3q/SsSCtlrw0gXsYLIpZ0cJpAo1",
+    HOME_SCREEN_NAME: "Home Screen",
+    validateEmail: "^[^\\s]+([.-]?\\w+)*@\\w+([.-]?\\w+)*(.\\w{2,3})+$",
+    SignInNavText: "Sign In",
+    SignUpNavText: "Sign Up",
+    SignInButtonText: "Login",
+    SignUpButtonText: "Sign Up"
   }
 }
